302428d187cff3f06db7427cbccf37511f479f4a
MohammedShmhani <158126523+MohammedShmhani@users.noreply.github.com>
2025-06-24 05:19:08 -0700

    Add files via upload

---
+++ Day_2.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class Day_2 {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        // Ex 1
+//        System.out.println("Enter your role : ");
+//        String role=i.nextLine();
+//        if(role.equalsIgnoreCase("admin"))
+//            System.out.println("welcome admin");
+//        else if (role.equalsIgnoreCase("admin"))
+//            System.out.println("welcome superuser");
+//        else if (role.equalsIgnoreCase("user"))
+//            System.out.println("welcome user");
+//        else
+//            System.out.println("Invalid input");
+        //Ex 2
+//        System.out.println("Enter num :");
+//        int n1=i.nextInt();
+//        System.out.println("Enter num :");
+//        int n2=i.nextInt();
+//        System.out.println("Enter num :");
+//        int n3=i.nextInt();
+//        if(n1>n2 && n1>n3)
+//            System.out.println("First num is Bigger");
+//        else if (n2>n1 && n2>n3)
+//            System.out.println("Second num is bigger");
+//        else if (n3>n1 && n3>n2)
+//            System.out.println("Third num is Bigger");
+//        else
+//            System.out.println("You enterd same value try again");
+        //Ex 3
+//        System.out.println("Enter num from 1 to 7");
+//        int x=i.nextInt();
+//        switch (x){
+//            case 1 ->{
+//                System.out.println("Sunday");
+//            }
+//            case 2 ->{
+//                System.out.println("Monday");
+//            }
+//            case 3 ->{
+//                System.out.println("Tuesday");
+//            }
+//            case 4 ->{
+//                System.out.println("Wednesday");
+//            }
+//            case 5 ->{
+//                System.out.println("Thursday");
+//            }
+//            case 6 ->{
+//                System.out.println("Friday");
+//            }
+//            case 7 ->{
+//                System.out.println("Saturday");
+//            }
+//            default -> {
+//                System.out.println("Invalid input");
+//            }
+//        }
+
+//        //Ex 4
+//        System.out.print("Enter your numeric score: ");
+//        int score = i.nextInt();
+//        String grade = null;
+//        if (score >= 90 && score <= 100) {
+//            grade = "A";
+//        } else if (score >= 80 && score <= 89) {
+//            grade = "B";
+//        } else if (score >= 70 && score <= 79) {
+//            grade = "C";
+//        } else if (score >= 60 && score <= 69) {
+//            grade = "D";
+//        } else {
+//            grade = "F";
+//        }
+//        System.out.println("Numeric Score: " + score);
+//        System.out.println("Letter Grade: " + grade);
+//    }
+//}
+
+//Ex5
+//        System.out.println("Enter your age: ");
+//        int age =i.nextInt();
+//        if(age<13)
+//            System.out.println("Child");
+//        else if (age>=13 && age <=19)
+//            System.out.println("Teenager");
+//        else if (age>=20)
+//            System.out.println("Adult");
+//        else
+//            System.out.println("Invald input");
+
+
+    }}
