5e068264194da660b7c996bd7915800df86b07f7
MohammedShmhani <158126523+MohammedShmhani@users.noreply.github.com>
2025-06-30 02:40:12 -0800

    Add files via upload

---
+++ Week2_Day_2.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Week2_Day_2 {
+    public static void main(String args[]) {
+        //Ex 1
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Input the first number :");
+//        double x = input.nextDouble();
+//        System.out.print("Input the second number :");
+//        double y = input.nextDouble();
+//        System.out.print("Input the third number :");
+//        double z = input.nextDouble();
+//        System.out.println("The smallest value is " + smallest(x, y, z));
+
+        // Ex 2
+
+//        System.out.print("Input the  number :");
+//        check(input.nextInt());
+
+        //Ex 3
+//
+//        System.out.println("Enter the pass with these conditions\n" + "A password must have at least ten characters.  \nA" + "password consists of only letters and digits.  \nA" + "password must contain at least two digits. ");
+//        System.out.println("Password is : " + validatay_Pass(input.nextLine()));
+
+    }
+
+    //Ex 1
+//    static double smallest(double x, double y, double z) {
+//        return Math.min(Math.min(x, y), z);
+//    }
+
+    //Ex 2
+//    static void check(int num) {
+//        if (num > 0)
+//            System.out.println(num + " is positive");
+//        else if (num < 0)
+//            System.out.println(num + " is nigitive");
+//        else
+//            System.out.println(num + " is zero");
+//
+//    }
+
+    //Ex 3
+
+//    static String validatay_Pass(String pass) {
+//        if (pass.length() < 8)
+//            return "Not valid";
+//        String symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+//        boolean hasSymbol = false;
+//        for (int i = 0; i < symbols.length(); i++) {
+//            if (pass.contains(String.valueOf(symbols.charAt(i)))) {
+//                hasSymbol = true;
+//                break;
+//            }
+//        }
+//        if (hasSymbol)
+//            return "Not valid";
+//
+//        int count = 0;
+//        for (int i = 0; i < pass.length(); i++) {
+//            if (Character.isDigit(pass.charAt(i))) count++;
+//
+//        }
+//        if (count < 2)
+//            return "Not valid";
+//
+//        return "valid";
+//
+//    }
+//
+
+}
